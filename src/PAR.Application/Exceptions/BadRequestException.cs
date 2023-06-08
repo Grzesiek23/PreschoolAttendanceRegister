aae0a7fb9910fec52f4e7519e7af30811435812c
Grzesiek <g.pasko@gmail.com>
2023-06-08 18:48:52 +0200

    BadRequestException.cs

---
+++ src/PAR.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,12 @@
+﻿namespace PAR.Application.Exceptions;
+
+public class BadRequestException : Exception
+{
+    public BadRequestException(string message) : base(message)
+    {
+    }
+
+    public BadRequestException(string commandName, string message) : base($"[{commandName}] {message}")
+    {
+    }
+}
