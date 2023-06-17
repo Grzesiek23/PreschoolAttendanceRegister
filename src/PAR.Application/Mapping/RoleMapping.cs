53e7ef6b2d78ccb6699712d4a26a0bb517eb7773
Grzesiek <g.pasko@gmail.com>
2023-06-17 22:07:13 +0200

    RoleMapping

---
+++ src/PAR.Application/Mapping/RoleMapping.cs
@@ -0,0 +1,16 @@
+﻿using PAR.Contracts.Dtos;
+using PAR.Domain.Entities;
+
+namespace PAR.Application.Mapping;
+
+public static class RoleMapping
+{
+    public static RoleDto AsDto(this ApplicationRole entity)
+    {
+        return new RoleDto
+        {
+            Id = entity.Id,
+            Name = entity.Name
+        };
+    }
+}
