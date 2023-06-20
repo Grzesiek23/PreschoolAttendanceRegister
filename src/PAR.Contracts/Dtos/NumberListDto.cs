bec153801e60527513ddd784a76e13542f1e3ae3
Grzesiek <g.pasko@gmail.com>
2023-06-20 11:39:02 +0200

    NumberListDto

---
+++ src/PAR.Contracts/Dtos/NumberListDto.cs
@@ -0,0 +1,7 @@
+﻿namespace PAR.Contracts.Dtos;
+
+public class NumberListDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ src/par-app/src/app/models/numberList.ts
@@ -0,0 +1,4 @@
+﻿export interface NumberList {
+    id: number;
+    name: string;
+}
